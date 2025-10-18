29e1329b2ebbe365c0a32fd3a93d01b904c6c33a
pbobell <pbobell@users.noreply.github.com>
2025-10-18 11:32:31 -0700

    Added frame rate throttle and a player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
@@ -8,13 +9,14 @@ def main():
     dt = 0
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    #surface = pygame.Surface()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
     print("Starting Asteroids!")
